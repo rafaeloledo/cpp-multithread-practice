55ce6c8c8c443f95350322b54088f384e07bfdf3
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-04-02 12:15:16 -0300

    chore(cpp): update fizzbuzz.cpp

--- cpp/fizz_buzz.cpp
+++ cpp/fizz_buzz.cpp
@@ -1,18 +1,29 @@
 #include <thread>
 #include <iostream>
+#include <vector>
+#include <string>
 
 class FizzBuzz {
 public:
+  std::vector<std::string> persons = {"Abdul", "Bart", "Claudia", "Divya"};
   void operator()() {
-    for (int i = 0; i <= 100;i++) {
+    for (int i = 1, j = 1; i <= 100; i++, j++) {
+      if (j > 4) j = 1;
+      std::string person;
+
+      if (j == 1) person = persons[0];
+      else if (j == 2) person = persons[1];
+      else if (j == 3) person = persons[2];
+      else person = persons[3];
+
       if (i % 3 == 0 && i % 5 == 0)
-        std::cout << "FizzBuzz\n";
+        std::cout << person << " says fizzbuzz!\n";
       else if (i % 3 == 0)
-        std::cout << "Fizz\n";
+        std::cout << person << " says fizz!\n";
       else if (i % 5 == 0)
-        std::cout << "Buzz\n";
+        std::cout << person << " says buzz!\n";
       else
-        std::cout << i << std::endl;
+        std::cout << person << " says " << i << std::endl;
     }
   }
 };
